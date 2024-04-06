7083f6a15b042d5f1712fd53b86430f505a20792
McNester <seraf5@bk.ru>
2024-04-06 14:55:00 +0500

    initiated the project

---
+++ bot.js
@@ -0,0 +1,15 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '6721671337:AAEYs_98w-z2TMbKFOwsaCnJNifJpEBMwN0'; // Replace with your own bot token
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', (msg) => {
+	const chatId = msg.chat.id;
+	const messageText = msg.text;
+
+	if (messageText === "/start") {
+		bot.sendMessage(chatId, 'Welcome!');
+	}
+
+})
+
