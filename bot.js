aacabc7c1b4c47815be919b1a4c88bc03ba8ec1b
McNester <seraf5@bk.ru>
2024-04-26 01:50:45 +0500

    two seconds

--- bot.js
+++ bot.js
@@ -14,18 +14,20 @@ module.exports = {
 			await bot.sendMessage(id, message, { parse_mode: 'Markdown' });
 
 		}
-	}, say: function(request) {
+	}, say: async function(request) {
 		const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
 		const { body } = request;
 		if (body.message) {
 			const { chat: { id }, text } = body.message;
-			const message = `FUCK`;
-
+			const message = `FIPI`;
+			await delay(2000)
 			// Send the initial message
-			bot.sendMessage(id, message, { parse_mode: 'Markdown' });
-			return 1;
+			await bot.sendMessage(id, message, { parse_mode: 'Markdown' });
 
 		}
 	}
+}
+function delay(ms) {
+	return new Promise(resolve => setTimeout(resolve, ms));
 }
 
