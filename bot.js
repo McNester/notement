38902b395077afbeea91bb33f630682283c207fa
McNester <seraf5@bk.ru>
2024-04-25 20:41:46 +0500

    fixed typo in a bot

--- bot.js
+++ bot.js
@@ -1,7 +1,8 @@
-rocess.env.NTBA_FIX_319 = 'test';
+process.env.NTBA_FIX_319 = 'test';
 const TelegramBot = require('node-telegram-bot-api');
 
 module.exports = {
+
 	startBot: async => (){
 		const bot = new TelegramBot(process.env.TELEGRAM_TOKEN);
 		const { body } = request;
