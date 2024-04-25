d5733ab754bbf03224e04062eebed8b5afb36460
McNester <seraf5@bk.ru>
2024-04-25 22:17:38 +0500

    adjust the time

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ module.exports = {
 
 			// Schedule sending the delayed message after 5 minutes (300,000 milliseconds)
 			setTimeout(sendDelayedMessage, 60000); // 5 minutes * 60 seconds * 1000 milliseconds
-			await new Promise(resolve => setTimeout(resolve, 5 * 60 * 1000)); // Wait for 5 minutes
+			await new Promise(resolve => setTimeout(resolve, 60000)); // Wait for 5 minutes
 			await sendDelayedMessage(); // Send the delayed message
 		}
 	}
