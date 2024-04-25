d9fc34587b1b8fc70885952980c17e09291dca05
McNester <seraf5@bk.ru>
2024-04-26 01:12:40 +0500

    changed sintax error

--- api/webhook.js
+++ api/webhook.js
@@ -6,7 +6,7 @@ module.exports = async (request, response) => {
 	try {
 
 		await startBot(request);
-		nodeSchedule.scheduleJob(Date.now() + 1000, function await() {
+		nodeSchedule.scheduleJob(Date.now() + 1000, async function() {
 			await say(request)
 		});
 		/*
