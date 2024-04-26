f91abf6edab9490b6f78accfa6d464a30b59199e
McNester <seraf5@bk.ru>
2024-04-26 13:34:51 +0500

    trying crono

--- api/webhook.js
+++ api/webhook.js
@@ -3,6 +3,17 @@ const { startBot, say } = require('../bot');
 const nodeSchedule = require('node-schedule');
 // Schedule 'say' to be called every 10 minutes
 let lastRequest = null;
+const job = new CronJob('*/1 * * * *', async function() {
+	console.log('Checking if scheduler should execute...');
+	try {
+		console.log('Executing scheduled task...');
+		await say(lastRequest);
+		console.info(lastRequest);
+	} catch (error) {
+		console.error('Error in scheduled say() execution:', error);
+	}
+}
+}, null, true, 'America/New_York'); // Replace 'America/New_York' with your time zone
 
 //nodeSchedule.scheduleJob('*/1 * * * *', async () => {
 /*
